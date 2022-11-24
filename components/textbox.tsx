9623a506cb6b882d558ea21068c2aa2d2e051feb
KUD <kasa7qi@gmail.com>
2022-11-24 17:30:38 +0900

    Build an ugly demostration, only view, no logic

---
+++ components/textbox.tsx
@@ -0,0 +1,13 @@
+export default function Textbox() {
+    return (
+        <div className="border border-blue-200 rounded p-6 my-4 w-96 dark:border-gray-800 bg-blue-50 dark:bg-blue-opaque">
+          <p className="text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100">
+            Girl
+          </p>
+
+          <p className="my-1 text-gray-800 dark:text-gray-200">
+            Time to go to school now.
+          </p>
+        </div>
+    );
+}
